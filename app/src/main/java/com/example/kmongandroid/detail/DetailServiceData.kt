a08a11181a5e1c23272e8bf7b424cc106b48e93b
jinyand <jiiin97@naver.com>
2020-11-30 21:54:15 +0900

    [hyunjin] 상세보기 화면 완성

---
+++ app/src/main/java/com/example/kmongandroid/detail/DetailServiceData.kt
@@ -0,0 +1,9 @@
+package com.example.kmongandroid.detail
+
+data class DetailServiceData(
+    val img : String,
+    val title : String,
+    val star : String,
+    val review : String,
+    val money : String
+)
